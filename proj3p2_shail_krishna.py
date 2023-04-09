42ea55d45b4fda141592b8d039fad2e1549f71a2
sshah115 <shailshah489@gmail.com>
2023-04-09 01:29:36 -0400

    #2 Obstacle check functionality added

--- proj3p2_shail_krishna.py
+++ proj3p2_shail_krishna.py
@@ -12,6 +12,7 @@ github link : https://github.com/sshah115/A-star-turtlebot.git
 # Importing required modules/libraries
 import numpy as np
 import cv2 as cv
+import copy
 
 # Function to generate map with obstacles using cv functions
 def genMap():
@@ -37,9 +38,48 @@ def genMap():
   
     return cv.resize(arena, (int(6000/scaleFac),int(2000/scaleFac)))
 
+
+# Checking coordinates if it lies in obstacle space
+def obstacle_check(node_c):
+    # This function will check if the points are okay, on border or
+    # inside obstacle
+
+    node = copy.deepcopy(node_c)
+
+    xPt = int(node[0]/scaleFac)
+
+    yPt = int((2000 - node[1])/scaleFac)
+
+    if yPt < int(2000/scaleFac) and xPt < int(6000/scaleFac):
+
+        print(f"The pixel value of coordinate {(node[0], node[1])} is: ", canvas[yPt, xPt])
+
+        if canvas[yPt, xPt][0] == 0 and canvas[yPt, xPt][1] == 0 and canvas[yPt, xPt][2] == 0:
+            status = False
+        elif canvas[yPt, xPt][0] == 255 and canvas[yPt, xPt][1] == 255 and canvas[yPt, xPt][2] == 255:
+            status = True
+        else:
+            status = True   
+
+    else:
+        status = True 
+        print("Out of arena")
+
+    cv.circle(canvas, (xPt,yPt), 1 , (0, 0, 255),-1) 
+
+    return status
+
 scaleFac = 5
 obsClea = 110
 canvas = genMap()
+
+node = [1500, 1000]
+stat = obstacle_check(node)
+
+if stat:
+    print("Obstacle")
+else:
+    print("Good to go")
 
 cv.imshow("Preview",canvas)
 cv.waitKey(0)
